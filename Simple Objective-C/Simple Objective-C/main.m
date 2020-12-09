abb9b61e7799c5974c0e36d2921a006f39d5a111
Fedor Filimonov <f.filimonov@gmail.com>
2020-12-09 19:41:34 +0300

    Initiation

---
+++ Simple Objective-C/Simple Objective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Simple Objective-C
+//
+//  Created by Федор Филимонов on 09.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
